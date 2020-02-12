3bbfb030ea4c06506c7f490e6cdc432d1773ba22
Istvan Lazar <istvanlaz@gmail.com>
2020-02-12 17:58:17 +0100

    adding debugg gems

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'uglifier'
 gem 'webpacker'
 
 group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
   gem 'web-console', '>= 3.3.0'
 end
 
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,33 +1,7 @@
 class PhotosController < ApplicationController
   def index
-    @lifts = Lift.all
   end
 
   def show
-    @lifts = Lift.all
-    @lift = Lift.find(params[:id])
-  end
-
-  def new
-    @lift = Lift.new
-  end
-
-  def edit
-    @lift = Lift.find(params[:id])
-  end
-
-  def create
-    @lift = Lift.new(lift_params)
-    if @lift.save
-      redirect_to lifts_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def lift_params
-    params.require(:lift).permit(:first_name, :family_name, :places_available, :contact, :rallying_point, :departure, :return)
   end
 end
